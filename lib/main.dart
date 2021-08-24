adabbe15effd786599c751455e9820b11711fe33
Abhay Chaudhary <chaudharyabhay111@gmail.com>
2021-08-24 20:55:57 +0530

    debug

--- lib/main.dart
+++ lib/main.dart
@@ -71,6 +71,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'DeliMeals',
       theme: ThemeData(
         primarySwatch: Colors.pink,
